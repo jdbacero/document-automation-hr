f8674f6e0361d28b31b1a12b6027f059a0349f29
jdbacero <jdbacero@gmail.com>
2022-07-13 03:30:23 +0800

    vue folder structure revised.

--- resources/js/Shared/Navigation/SideNav.vue
+++ resources/js/Shared/Navigation/SideNav.vue
@@ -2,7 +2,7 @@
     <nav class="text-right">
         <div class="text-center">
             <h1 class="font-bold uppercase p-4 border-b border-gray-100">
-                <Link href="/" class="hover:text-cyan-500" preserve-scroll="true">EmpireOne</Link>
+                <Link href="/" class="hover:text-cyan-500" preserve-scroll="true">Company Name</Link>
             </h1>
             <div class="px-4 cursor-pointer md:hidden" id="burger" @click="showMenu">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
